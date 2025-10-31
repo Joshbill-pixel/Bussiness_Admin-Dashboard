6788069acecaa30f5b16cba71eba58a472ed9567
Ejiofor Joshua <ejioforjo01@gmail.com>
2025-10-31 10:11:38 +0100

    saving changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  base: '/Bussiness_Admin-Dashboard/',
+})
+
 
 
 // import { defineConfig } from 'vite';
